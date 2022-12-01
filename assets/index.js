b5dbdd4468d7aaab82c1ffd4e077b40e593aa569
Krishna Bhandari <115021507+ibhandarikrishna@users.noreply.github.com>
2022-12-01 21:19:10 +0530

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+
+
+
+    fadeOut(); 
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+  }
+  function fadeOut(){
+    setTimeout(loader,4000);
+  }
+  
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Group- ps9
+Members- 	Krishna Bhandari (22MCA10139)
+		Ankush Kumar Jha (22MCA10172)
+		Nikhil Thakur  (22MCA10042)
+		Ashwini Kumar Tiwari  (22MCA10161)
+
+Run MergeSort.html
